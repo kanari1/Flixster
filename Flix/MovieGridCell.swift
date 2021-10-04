e9d21a93c9c98d2b688ab25cfb9daf9f670a7da2
kanari1 <ktrnajang5914@gmail.com>
2021-10-04 12:52:02 -0700

    implemented grid layout of movie posters

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Katrina J. on 10/1/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
